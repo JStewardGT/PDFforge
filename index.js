3474e5d4156f39a66be146ab9365e349a079bcdd
Johan Galeano <jgaleano@ivolucion.com>
2024-07-08 15:04:41 -0500

    vercel

--- index.js
+++ index.js
@@ -3,7 +3,24 @@ const generatePdf = require("./pdf/generate-pdf");
 
 const app = express();
 
+const PORT = process.env.PORT || 3000;
+
 app.use(express.json());
+
+app.use("/", async (req, res) => {
+  const htmlResponse = `
+    <html>
+      <head>
+        <title>Generate PDF</title>
+      </head>
+      <body>
+        <h1>Generate PDF</h1>
+        <p>Esto es una prueba para vercel</p>
+      </body>
+    </html>
+  `;
+  res.send(htmlResponse);
+})
 
 app.use("/generate-pdf", async (req, res) => {
   const pdfBuffer = await generatePdf({ url: req.body.url });
@@ -17,7 +34,6 @@ app.use("/generate-pdf", async (req, res) => {
     .end(pdfBuffer);
 });
 
-const PORT = process.env.PORT ?? 3000;
 
 app.listen(PORT, () => {
   console.log(`Server is listening on port http://localhost:${PORT}`);
